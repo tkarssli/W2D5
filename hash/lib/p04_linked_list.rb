f791159f00cf45db0b4b963c7de71608851063d2
tkarssli <tkarssli@gmail.com>
2019-01-18 15:38:21 -0800

    Add p04_linked_list.rb Linked List class

--- hash/lib/p04_linked_list.rb
+++ hash/lib/p04_linked_list.rb
@@ -20,7 +20,13 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = tail
+    @tail.prev = head
   end
 
   def [](i)
@@ -29,34 +35,71 @@ class LinkedList
   end
 
   def first
+    head.next unless head.next == tail
   end
 
   def last
+    tail.prev unless tail.prev == head
   end
 
   def empty?
+    head.next == tail && tail.prev == head
   end
 
   def get(key)
+    each do |node|
+      return node.val if node.key == key
+    end
   end
 
   def include?(key)
+    each do |node|
+      return true if node.key == key
+    end
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    last_node = tail.prev
+    
+    last_node.next = new_node
+    new_node.prev = last_node
+    new_node.next = tail
+    tail.prev = new_node
   end
 
   def update(key, val)
+      each do |node|
+        node.val = val if node.key == key
+      end
   end
 
   def remove(key)
+    each do |node|
+      if node.key == key
+        last_node = node.prev
+        next_node = node.next
+    
+        last_node.next = next_node
+        next_node.prev = last_node
+      end
+    end
   end
 
-  def each
+  def each(&prc)
+    curr_node = head.next
+    until curr_node == tail
+      prc.call(curr_node)
+      curr_node = curr_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
+
+  private
+  attr_reader :head, :tail
 end
