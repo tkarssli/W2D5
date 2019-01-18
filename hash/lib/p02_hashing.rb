86448e76d2645ef8c8c31e3068bd6c7b4c240213
tkarssli <tkarssli@gmail.com>
2019-01-18 15:37:11 -0800

    Add p02_hashing.rb to include monkeypatching Array, String, and Hash classes

--- hash/lib/p02_hashing.rb
+++ hash/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    hash_counter = 0
+    self.each_with_index do |el, i|
+      hash_counter = hash_counter ^ (el * i).hash
+    end
+    hash_counter
   end
 end
 
 class String
   def hash
+    hash_counter = 0
+    self.chars.each_with_index do |el ,i|
+      hash_counter = hash_counter ^ (el.ord * i).hash
+    end
+    hash_counter
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_counter = 0
+    self.each do |key, value|
+      hash_counter = hash_counter ^ key.hash ^ value.hash
+    end
+    hash_counter
   end
 end
